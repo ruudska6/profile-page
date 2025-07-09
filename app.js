a6c30c09a18a65f02a028cd7123de89e3aa0f319
ruudska6 <feels19@naver.com>
2025-07-09 18:13:24 +0900

    remove 불 필요한 코드

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-document.querySelectorAll(".side-nav a").forEach((anchor) => {
-  anchor.addEventListener("click", function (e) {
-    e.preventDefault();
-    const target = document.querySelector(this.getAttribute("href"));
-    target.scrollIntoView({ behavior: "smooth" });
-  });
-});
-
 const navLinks = document.querySelectorAll(".side-nav a");
 const sections = document.querySelectorAll("section");
 
