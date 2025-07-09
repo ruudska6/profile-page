112cb1d1057d86baf64cdd326a0f4538b84e56d2
ruudska6 <feels19@naver.com>
2025-07-09 16:01:20 +0900

    디자인 수정

---
+++ app.js
@@ -0,0 +1,28 @@
+document.querySelectorAll(".side-nav a").forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute("href"));
+    target.scrollIntoView({ behavior: "smooth" });
+  });
+});
+
+const navLinks = document.querySelectorAll(".side-nav a");
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop - 100;
+    if (pageYOffset >= sectionTop) {
+      current = section.getAttribute("id");
+    }
+    console.log(current);
+  });
+
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+    if (link.getAttribute("href").includes(current)) {
+      link.classList.add("active");
+    }
+  });
+});
